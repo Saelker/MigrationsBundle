f022f5396588d2266fe5354d1de084aeee333a68
malte <malte.frerichs@gmx.net>
2018-01-24 11:16:55 +0000

    No Tree Dependencies, only simple child / parent

--- Helper/DependencyHelper.php
+++ Helper/DependencyHelper.php
@@ -6,10 +6,6 @@ use Saelker\MigrationsBundle\Util\ImportFile;
 
 class DependencyHelper
 {
-	/**
-	 * array
-	 */
-	private $parentImportFiles = [];
 
 	/**
 	 * @var array
@@ -32,52 +28,22 @@ class DependencyHelper
 			$migrationFile = $importFile->getInstance();
 
 			$this->availableImportFiles[$migrationFile->getClassName()] = $importFile;
+		}
 
-			if (!$migrationFile->getDependency()) {
-				$this->parentImportFiles[$migrationFile->getClassName()] = $importFile;
+		foreach ($importFiles as $importFile) {
+			$migrationFile = $importFile->getInstance();
+
+			if (($dependency = $migrationFile->getDependency()) && array_key_exists($dependency, $this->availableImportFiles)) {
+				$this->newOrderedImportFiles[] = $this->availableImportFiles[$dependency];
+				unset($this->availableImportFiles[$dependency]);
 			}
+
+			$this->newOrderedImportFiles[] = $importFile;
+			unset($this->availableImportFiles[$migrationFile->getClassName()]);
 		}
 
-		/**
-		 * @var string $className
-		 * @var ImportFile $importFile
-		 */
-		foreach ($this->parentImportFiles as $className => $importFile) {
-			$this->handleImportFile($importFile);
-		}
 
 		return $this->newOrderedImportFiles;
-	}
-
-	/**
-	 * @param ImportFile $importFile
-	 */
-	private function handleImportFile(ImportFile $importFile): void
-	{
-		$migrationFile = $importFile->getInstance();
-
-		if (array_key_exists($migrationFile->getClassName(), $this->availableImportFiles)) {
-			unset($this->availableImportFiles[$migrationFile->getClassName()]);
-			$this->newOrderedImportFiles[] = $importFile;
-
-			// Find Same Dependency Resolutions
-			$sameDependencyResolutions = array_filter($this->availableImportFiles, function (ImportFile $importFile) use ($migrationFile) {
-				return $importFile->getInstance()->getDependencyResolution() == $migrationFile->getDependencyResolution();
-			});
-
-			foreach ($sameDependencyResolutions as $sameDependencyResolution) {
-				$this->handleImportFile($sameDependencyResolution);
-			}
-
-			// Find Depended Migrations
-			$dependedMigrations = array_filter($this->availableImportFiles, function (ImportFile $importFile) use ($migrationFile) {
-				return $importFile->getInstance()->getDependency() == $migrationFile->getDependencyResolution();
-			});
-
-			foreach ($dependedMigrations as $dependedMigration) {
-				$this->handleImportFile($dependedMigration);
-			}
-		}
 	}
 
 }
