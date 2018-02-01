c4d7b2a28a6ea31a66317f9204ae0a01472661bc
pw <pw@saelker.de>
2018-02-01 09:04:34 +0100

    added install directory option to migrate full

--- Command/MigrationsMigrateFullCommand.php
+++ Command/MigrationsMigrateFullCommand.php
@@ -27,6 +27,12 @@ class MigrationsMigrateFullCommand extends ContainerAwareCommand
 				InputOption::VALUE_OPTIONAL,
 				'If false or null all directories are used',
 				false)
+			->addOption(
+				'install-directory',
+				null,
+				InputOption::VALUE_OPTIONAL,
+				'will be used for initial installation',
+				'')
 			->setDescription('Starts developer migrations');
 	}
 
@@ -34,6 +40,7 @@ class MigrationsMigrateFullCommand extends ContainerAwareCommand
 	 * @param InputInterface $input
 	 * @param OutputInterface $output
 	 * @return int|null|void
+	 * @throws \Exception
 	 */
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
@@ -45,6 +52,10 @@ class MigrationsMigrateFullCommand extends ContainerAwareCommand
 		$directory = false;
 		if ($input->getOption('select-directory')) {
 			$directory = $io->choice('Select a Directory', $directoryHelper->getSourceDirectories($migrationsManager->getDirectories()));
+		}
+
+		if ($input->getOption('install-directory')) {
+			$directory = $input->getOption('install-directory');
 		}
 
 		$migrationsManager->migrateFull($io, $directory);
