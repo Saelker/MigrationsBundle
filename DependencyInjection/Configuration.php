f244f4fd66bfefd4dcc613a4b635d1c70e6d6770
Wulli <wulli2005@gmx.de>
2020-06-18 11:19:53 +0200

    updated treebuilder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,10 +17,10 @@ class Configuration implements ConfigurationInterface
 	 */
 	public function getConfigTreeBuilder()
 	{
-		$treeBuilder = new TreeBuilder();
-		$rootNode = $treeBuilder->root('saelker_migrations');
+		$treeBuilder = new TreeBuilder('saelker_migrations');
 
-		$rootNode
+		$treeBuilder
+			->getRootNode()
 			->children()
 			->arrayNode('directories')
 			->prototype('scalar')->end()
